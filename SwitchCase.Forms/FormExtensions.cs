a697310a038a16dd769204e5aa1c48e2926108d9
Manuel J <manuel.jordan@gmx.net>
2021-12-12 22:09:11 +0100

    Create FormExtensions.cs

---
+++ SwitchCase.Forms/FormExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase.Forms
+{
+    public static class FormExtensions
+    {
+        public static void CloseDialog(this Form form, DialogResult dialogResult = DialogResult.OK)
+        {
+            form.DialogResult = dialogResult;
+            form.Close();
+        }
+    }
+}
