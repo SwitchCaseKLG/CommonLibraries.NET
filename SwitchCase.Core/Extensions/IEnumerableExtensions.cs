33bafcec08b374d429f81c1ce36efaa82398e204
Manuel J. <manuel.jordan@gmx.net>
2024-07-21 16:21:12 +0200

    rename extension

--- SwitchCase.Core/Extensions/IEnumerableExtensions.cs
+++ SwitchCase.Core/Extensions/IEnumerableExtensions.cs
@@ -13,7 +13,7 @@
             return items.Select(i => new KeyValuePair<Tkey, T>(keySelector(i), i)).Dedupe(i => i.Key).ToDictionary(i => i.Key, i => i.Value);
         }
 
-        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        public static IEnumerable<T> DistinctByKey<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
         {
             HashSet<TKey> knownKeys = new();
             foreach (T element in source)
